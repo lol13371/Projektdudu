c4451c8e1adc0ba2b5028f04efd2b3c7800fc140
lol13371 <117176678+lol13371@users.noreply.github.com>
2022-12-21 07:37:50 +0600

    Update main.b607e494.chunk.js

--- static/js/main.b607e494.chunk.js
+++ static/js/main.b607e494.chunk.js
@@ -651,7 +651,7 @@
                                             className: "mint-live text-shadow",
                                             children: ["UNTIL ", Object(N.jsx)("span", {
                                                 className: "text-primary",
-                                                children: "25 DEC 12:00H"
+                                                children: "22 DEC 12:00H"
                                             })]
                                         }), Object(N.jsxs)("p", {
                                             className: "priceSale mt-3",
